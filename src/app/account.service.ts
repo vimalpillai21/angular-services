08693a42087103d213508ccaa5352c9fa0ce6a23
Vimal Pillai <vimalpillai@goodbits.in>
2019-04-28 07:58:39 +0530

    cross communication

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,5 +1,5 @@
 import { LoggingService } from './logging.service';
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable()
 export class AccountService{
@@ -17,7 +17,7 @@ export class AccountService{
           status: 'unknown'
         }
       ];
-
+      statusUpdated = new EventEmitter<string>();
       constructor(private loggingService: LoggingService){}
       
       addAccount(name:string, status:string){
